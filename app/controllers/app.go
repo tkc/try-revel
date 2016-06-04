65f4d9f9f3493be2404134210ed930211ba0450a
tkc <loosefingers@gmail.com>
2016-06-04 20:46:10 +0900

    Update

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,11 +1,20 @@
 package controllers
 
 import "github.com/revel/revel"
+// import "net/http"
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) TryPage() revel.Result {
+  return c.Render()
+}
+
+func (c App) JsonReturn() revel.Result {
 	return c.Render()
 }
